e14d83c639caa1994f42a291a53cb876af1a1879
Luciana Viviani Gallo <lucianavivianigallo@gmail.com>
2021-11-03 13:28:28 +0000

    for loops arrays

---
+++ packing-list.js
@@ -0,0 +1,6 @@
+const packingList = ["bowls", "plates", "pots", "pans", "eating utensils", "glasses", "cups", "cooking utensils"];
+
+console.log("Kitchen stuff to pack:"); // it will print only this introductory sentence
+for (let i = 0; i < packingList.length; i++) {
+  console.log(packingList[i]); // do not forget the array and [i], to list all elements
+}
