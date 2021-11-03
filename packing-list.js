f66e44f769bfd87e26c791494635b9dc7295310a
Luciana Viviani Gallo <lucianavivianigallo@gmail.com>
2021-11-03 13:31:09 +0000

    \while loops arrays

--- packing-list.js
+++ packing-list.js
@@ -1,6 +1,20 @@
+//for loops
+
+/* 
 const packingList = ["bowls", "plates", "pots", "pans", "eating utensils", "glasses", "cups", "cooking utensils"];
 
 console.log("Kitchen stuff to pack:"); // it will print only this introductory sentence
 for (let i = 0; i < packingList.length; i++) {
   console.log(packingList[i]); // do not forget the array and [i], to list all elements
+}
+*/
+
+// while loops
+const packingList = ["bowls", "plates", "pots", "pans", "eating utensils", "glasses", "cups", "cooking utensils"];
+
+console.log("Kitchen stuff to pack:");
+let i = 0;
+while (i < packingList.length) {
+  console.log(packingList[i]);
+  i++;
 }
